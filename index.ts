deff257b447a76042b2cd317aa02146a6f9afe72
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-02-02 10:32:19 -0800

    Sample

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
-export { createTextAnalyticsClient } from "./src/textAnalyticsClient";
-export { TextAnalyticsClient } from "./src/types/types";
+import { createTextAnalyticsClient } from "./src/textAnalyticsClient";
+import { DefaultAzureCredential } from "@azure/identity";
+
+async function main() {
+  const client = createTextAnalyticsClient(
+    new DefaultAzureCredential(),
+    "https://localhost:3000"
+  );
+
+  const documents = [
+    { id: "First", text: "This is a test text" },
+    { id: "Second", text: "Este es un texto de prueba" },
+    { id: "third", text: "asdfasdf asdfasdfasdfer" },
+  ];
+
+  const languagesResult = await client.request("POST /languages", {
+    documents,
+  });
+
+  console.log(
+    languagesResult.data.documents.map((d) => console.log(d.detectedLanguage))
+  );
+}
